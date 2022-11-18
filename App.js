4571173d1fdcebf982bbd6b20a2935be18c2b3d0
Cloudxz1 <vanesa.briones@alumnos.ucn.cl>
2022-11-18 00:02:15 -0300

    Presentacion Componentes

--- App.js
+++ App.js
@@ -1,40 +1,17 @@
-import React from "react";
-import { View, StyleSheet, ToastAndroid, Button, StatusBar, Alert } from "react-native";
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import {StyleSheet,StatusBar} from 'react-native';
 
-const App = () => {
-
-
-
-  const ToastExample = () => {
-    ToastAndroid.show("Este es un toast", ToastAndroid.SHORT);
-  };
-
-
-  const AlertExample = () =>
-    Alert.alert(
-      "Esta es una Alerta", "Aqui puedes poner la descripcion -mensaje",
-      [
-        {
-          text: "Cancelar",
-          onPress: () => console.log("Cancelar presionado"),
-          style: "cancel"
-        },
-        
-        { 
-          text: "aceptar", onPress: () => console.log("aceptar presionado")
-        }
-      ]
+class App extends Component {
+  render() {
+    return (
+      <WebView
+        source={{ uri: 'https://google.com' }}
+        style={{ marginTop: 20 }}
+      />
     );
-
-
-  return (
-    <View style={styles.container}>
-      <Button title="Toast" onPress={() => ToastExample()} />
-
-      <Button title="Alert" onPress={() => AlertExample()}/>
-    </View>
-  );
-};
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
